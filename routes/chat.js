9d670b8f76a6bf6239de429d40633b63602a8ece
mahfuj10 <mahfujglobal@gmail.com>
2022-05-23 10:12:40 +0600

    added all files

---
+++ routes/chat.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const router = express.Router();
+const { MongoClient } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId;
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39aol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const database = client.db("Leaves");
+const usersChat = database.collection('chats');
+
+client.connect();
+
+
+// post user message
+
+router.post('/', async (req, res) => {
+    try {
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+// get room data 
+router.get('/:room', async (req, res) => {
+    try {
+        const roomId = req.params.room;
+        if (isNaN(roomId) !== true) {
+            const query = { roomId: parseInt(roomId) };
+            res.send(await usersChat.find(query).toArray());
+        };
+    } catch (err) {
+        res.status(500).json({ message: 'There was errror in server' })
+    }
+});
+
+// get all data
+router.get('/', async (req, res, next) => {
+    try {
+        res.send(await usersChat.find({}).toArray());
+    } catch (err) {
+        next(err)
+    }
+});
+
+// delete message data
+router.put('/deletemessage/:id', async (req, res) => {
+    // try {
+    const messageId = req.params.id;
+    const query = { _id: ObjectId(messageId) };
+    const updatedDoc = { $set: { deleted: true } };
+    res.send(await usersChat.updateOne(query, updatedDoc));
+    // } catch (err) {
+    //     res.status(500).json({ message: 'There was errror in server' })
+    // }
+});
+
+
+//delete all message from room
+router.delete('/deleteallmessages/:roomId', async (req, res) => {
+    try {
+        const roomId = parseInt(req.params.roomId);
+        const query = { roomId: roomId };
+        res.send(await usersChat.deleteMany(query));
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+// mongodb connectiorsn
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.39aol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+// socket.io connection
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+
+    console.log("User connected with ", socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.roomId).emit("recive_message", data);
+    })
+
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data)
+    })
+
+    socket.on('deleteMessage', function (data) {
+        socket.to(data.roomId).emit("deleteMessage", data);
+    })
+
+
+    const users = {};
+
+    socket.on('login', function (data) {
+        // const uid = { userId: data?.userId };
+        users[socket.id] = data.loginUser?.uid
+        socket.broadcast.emit('user-connected', data.loginUser);
+
+        // socket.broadcast.emit("activeusers", users);
+    });
+
+
+    socket.on('addedUser', function (data) {
+        socket.emit('addedUser', data);
+        console.log(data)
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+        console.log(`User disconnected ${socket.id}`);
+    });
+
+
+
+
+
+    // socket.on('checkActive', id => {
+    //     socket.to(id).emit('isActive', id);
+    // })
+    // socket.on('activeUser', user => {
+    //     socket.broadcast.emit('receive_activeUser', user)
+    // })
+
+})
+
+// mongodb connection
+
+// import router
+const users = require('./routes/users');
+const chat = require('./routes/chat');
+const groups = require('./routes/groups');
+
+async function run() {
+
+    try {
+
+        app.use('/users', users);
+        app.use('/chat', chat);
+        app.use('/group', groups);
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(e => console.log(e)).finally();
+
+app.get("/", async (req, res) => {
+
+    try {
+        res.send("Leaves server is running...");
+    } catch (err) {
+        res.json({ message: 'server error' })
+    }
+
+})
+
+
+
+server.listen(port, () => {
+    console.log("my server is runningin port 5000")
+})
